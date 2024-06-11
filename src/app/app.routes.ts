72ec3a1142806ad3b16a090d27084009c5144cf3
jusalamanca <99748643+jusalamanca@users.noreply.github.com>
2024-06-10 22:13:12 -0500

    nada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
