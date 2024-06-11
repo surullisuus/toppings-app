a03f4e0cb23c140b3a27ee586b85fccbd40705b8
jusalamanca <99748643+jusalamanca@users.noreply.github.com>
2024-06-10 23:17:51 -0500

    cambion en dash board y ventas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { VentasComponent } from './ventas/ventas.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
     { path: 'sidebar', component: SidebarComponent },
+    { path: 'ventas', component: VentasComponent },
 ];
